d4cb8b72bae5204201706ad0de53d281963bd49d
Ismael Moral <jastertdc@gmail.com>
2019-10-28 20:10:50 +0100

    feature/symfony-di
    * We added symfony dependency injector in order clean a little bit the
    mess of index_hex.php

--- index_hex.php
+++ index_hex.php
@@ -2,84 +2,25 @@
 
 use BestThor\ScrappingMaster\Application\Service\RetrieveElementService;
 use BestThor\ScrappingMaster\Application\Service\RetrieveElementServiceArguments;
-use BestThor\ScrappingMaster\Application\UseCase\RetrieveElementDetailUseCase;
-use BestThor\ScrappingMaster\Application\UseCase\RetrieveElementDownloadUseCase;
-use BestThor\ScrappingMaster\Application\UseCase\RetrieveElementGeneralUseCase;
-use BestThor\ScrappingMaster\Application\UseCase\SaveElementInFileUseCase;
 use BestThor\ScrappingMaster\Infrastructure\DataTransformer\ElementGeneralCollectionDataTransformer;
-use BestThor\ScrappingMaster\Infrastructure\Factory\ElementDetailFactory;
-use BestThor\ScrappingMaster\Infrastructure\Factory\ElementDownloadFactory;
-use BestThor\ScrappingMaster\Infrastructure\Factory\ElementGeneralFactory;
-use BestThor\ScrappingMaster\Infrastructure\Parser\ElementDetailParser;
-use BestThor\ScrappingMaster\Infrastructure\Parser\ElementDownloadParser;
-use BestThor\ScrappingMaster\Infrastructure\Parser\ElementGeneralParser;
-use BestThor\ScrappingMaster\Infrastructure\Repository\GuzzleMTContentReaderRepository;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$homeUrl = 'http://www.mejortorrentt.org';
-
-$filmUrl = '/secciones.php?sec=descargas&ap=peliculas&p=%s';
-$downloadElementUrl = '/secciones.php?sec=descargas&ap=contar&tabla=peliculas&id=%s&link_bajar=1';
-$downloadElementTorrentUrl = '/uploads/torrents/peliculas/';
+/** @var ContainerBuilder $containerBuilder */
+$containerBuilder = require_once __DIR__ . '/config/dependencies.php';
 
 try {
-
-    // Element general
-    $elementGeneralFactory = new ElementGeneralFactory();
-    $elementGeneralParser = new ElementGeneralParser(
-        $elementGeneralFactory
-    );
-    $retrieveElementGeneralUseCase = new RetrieveElementGeneralUseCase(
-        $elementGeneralParser
-    );
-
-    // Element detail
-    $elementDetailFactory = new ElementDetailFactory(
-        __DIR__ . '/scrap-torrent'
-    );
-    $elementDetailParser = new ElementDetailParser(
-        $elementDetailFactory
-    );
-    $retrieveElementDetailUseCase = new RetrieveElementDetailUseCase(
-        $elementDetailParser
-    );
-
-    // Element download
-    $elementDownloadFactory = new ElementDownloadFactory(
-        $downloadElementTorrentUrl
-    );
-    $elementDownloadParser = new ElementDownloadParser(
-        $elementDownloadFactory
-    );
-    $retrieveElementDownloadUseCase = new RetrieveElementDownloadUseCase(
-        $elementDownloadParser
-    );
-
-    $guzzleMTContentReaderRepository = new GuzzleMTContentReaderRepository(
-        $homeUrl,
-        $filmUrl,
-        $downloadElementUrl
-    );
-
-    $saveElementInFileUseCase = new SaveElementInFileUseCase(
-        $guzzleMTContentReaderRepository
-    );
-
-    $retrieveElementService = new RetrieveElementService(
-        $retrieveElementGeneralUseCase,
-        $retrieveElementDetailUseCase,
-        $retrieveElementDownloadUseCase,
-        $guzzleMTContentReaderRepository,
-        $saveElementInFileUseCase
-    );
+    /** @var RetrieveElementService $retrieveElementService */
+    $retrieveElementService = $containerBuilder
+        ->get(RetrieveElementService::class);
 
     $retrieveElementServiceResponse = $retrieveElementService
         ->handle(
             new RetrieveElementServiceArguments(
                 1
             )
-        );
+    );
 
     if (!$retrieveElementServiceResponse->isSuccess()) {
         echo $retrieveElementServiceResponse->getError() . "\n";
