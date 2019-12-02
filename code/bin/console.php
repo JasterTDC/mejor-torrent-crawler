2489d6b0d36e18b6e5723fd8ea94aa09632df532
Ismael Moral <jastertdc@gmail.com>
2019-12-03 00:12:39 +0100

    feature/dependencies-cleaning
    * We remove the unused dependencies.

--- code/bin/console.php
+++ code/bin/console.php
@@ -1,11 +1,15 @@
 <?php
 
-use BestThor\ScrappingMaster\Infrastructure\Command\GeneralCrawlerCommand;
-use BestThor\ScrappingMaster\Infrastructure\Command\SeriesCrawlerCommand;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\Container;
+use BestThor\ScrappingMaster\Infrastructure\Command\SeriesCrawlerCommand;
+use BestThor\ScrappingMaster\Infrastructure\Command\GeneralCrawlerCommand;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotEnv = new Dotenv();
+$dotEnv->load(__DIR__ . '/../env/.env');
 
 /** @var Container $container */
 $container = require_once __DIR__ . '/../slim-fw/dependencies.php';
