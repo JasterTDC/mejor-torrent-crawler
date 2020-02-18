23cd7b78fc3cc775be6b05005bcd634374a8535c
Ismael Moral <jastertdc@gmail.com>
2020-02-18 20:01:03 +0100

    Bugfix
    * We remove get tag from general use case because we added this method
    in get element use case.

--- code/app/container/command.php
+++ code/app/container/command.php
@@ -1,14 +1,12 @@
 <?php
 
-use BestThor\ScrappingMaster\Application\UseCase\ElementGeneral\GetTagFromGeneralUseCase;
-use BestThor\ScrappingMaster\Application\UseCase\Notification\SendNotificationUseCase;
-use BestThor\ScrappingMaster\Infrastructure\Command\GetTagFromGeneralCommand;
-use BestThor\ScrappingMaster\Infrastructure\Command\SendNotificationCommand;
-use Symfony\Component\DependencyInjection\Reference;
-use BestThor\ScrappingMaster\Infrastructure\Command\SeriesCrawlerCommand;
-use BestThor\ScrappingMaster\Infrastructure\Command\GeneralCrawlerCommand;
-use BestThor\ScrappingMaster\Application\UseCase\ElementSeries\GetElementSeriesCollectionUseCase;
 use BestThor\ScrappingMaster\Application\UseCase\ElementGeneral\GetElementGeneralCollectionUseCase;
+use BestThor\ScrappingMaster\Application\UseCase\ElementSeries\GetElementSeriesCollectionUseCase;
+use BestThor\ScrappingMaster\Application\UseCase\Notification\SendNotificationUseCase;
+use BestThor\ScrappingMaster\Infrastructure\Command\GeneralCrawlerCommand;
+use BestThor\ScrappingMaster\Infrastructure\Command\SendNotificationCommand;
+use BestThor\ScrappingMaster\Infrastructure\Command\SeriesCrawlerCommand;
+use Symfony\Component\DependencyInjection\Reference;
 
 $container->register(
     SeriesCrawlerCommand::class,
@@ -19,11 +17,6 @@ $container->register(
     GeneralCrawlerCommand::class,
     GeneralCrawlerCommand::class
 )->addArgument(new Reference(GetElementGeneralCollectionUseCase::class));
-
-$container->register(
-    GetTagFromGeneralCommand::class,
-    GetTagFromGeneralCommand::class
-)->addArgument(new Reference(GetTagFromGeneralUseCase::class));
 
 $container->register(
     SendNotificationCommand::class,
