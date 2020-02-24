2e9e23f2069f36a343a5e7f95b517736f5bfa3de
Ismael Moral <jastertdc@gmail.com>
2020-02-24 23:20:02 +0100

    feature/parser-unit-testing
    * We add unit testing for element series detail factory.

---
+++ code/tests/Infrastructure/Factory/ElementSeries/ElementSeriesDetailFactoryTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BestThor\ScrappingMaster\Tests\Infrastructure\Factory\ElementSeries;
+
+use BestThor\ScrappingMaster\Domain\Series\ElementSeriesDetailCollection;
+use BestThor\ScrappingMaster\Infrastructure\Factory\ElementSeriesDetailFactory;
+use BestThor\ScrappingMaster\Tests\Domain\ElementSeries\ElementSeriesDetailArrayMother;
+use BestThor\ScrappingMaster\Tests\Domain\ElementSeries\ElementSeriesDetailCollectionArrayMother;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ElementSeriesDetailFactory
+ *
+ * @package BestThor\ScrappingMaster\Tests\Infrastructure\Factory\ElementSeries
+ * @author  Ismael Moral <jastertdc@gmail.com>
+ */
+final class ElementSeriesDetailFactoryTest extends TestCase
+{
+    /** @var ElementSeriesDetailFactory  */
+    protected ElementSeriesDetailFactory $factory;
+
+    protected function setUp(): void
+    {
+        $this->factory = new ElementSeriesDetailFactory();
+    }
+
+    public function testIfArrayDataIsValid(): void
+    {
+        $elementDetail = $this
+            ->factory
+            ->createFromRaw(
+                ElementSeriesDetailArrayMother::random()
+            );
+
+        $this->assertIsInt($elementDetail->getId());
+        $this->assertIsString($elementDetail->getName());
+        $this->assertIsString($elementDetail->getLink());
+        $this->assertInstanceOf(
+            \DateTimeImmutable::class,
+            $elementDetail->getCreatedAt()
+        );
+        $this->assertInstanceOf(
+            \DateTimeImmutable::class,
+            $elementDetail->getUpdatedAt()
+        );
+        $this->assertNull($elementDetail->getSeriesId());
+        $this->assertNull($elementDetail->getElementSeriesDownload());
+    }
+
+    public function testIfCollectionArrayIsValid(): void
+    {
+        $elementDetailCollection = $this
+            ->factory
+            ->createFromRawCollection(
+                ElementSeriesDetailCollectionArrayMother::random()
+            );
+
+        $this->assertInstanceOf(
+            ElementSeriesDetailCollection::class,
+            $elementDetailCollection
+        );
+        $this->assertGreaterThan(1, $elementDetailCollection->count());
+    }
+}
